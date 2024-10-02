a55c5be191d261d9e516bd747184311adfd1aa0f
arjunchuhan6 <arjunchuhan6@gmail.com>
2024-10-02 09:05:47 +0530

    Book Management Web App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import About from './components/about.jsx'
+import Contact from './components/Contact.jsx'
 import './index.css'
+import {createBrowserRouter,RouterProvider} from  'react-router-dom'
 
+//Creating Routing configuration 
+
+const appRouter=createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element:<About/>
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter}/>
   </StrictMode>,
 )
